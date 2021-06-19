c263a4874645e6c4efd2e3c60ac766efdca275e2
SlicedDice <60396325+SlicedDice@users.noreply.github.com>
2021-06-19 19:48:10 +0200

    Frog Character Controller
    Added a character controller for the frog

--- Assets/Scripts/Movement_Frog.cs
+++ Assets/Scripts/Movement_Frog.cs
@@ -4,19 +4,85 @@ using UnityEngine;
 
 public class Movement_Frog : MonoBehaviour
 {
-    public float jumpingStrength = 25f;
+    public float walkingSpeed = 1f;
+    public float turnSpeed = 1f;
 
-    // Update is called once per frame
+    public float jumpingStrength = 25f;
+    public float directionalJumpStrength = 1f;
+
+    private float startJumpTime;
+    private float maxJumpTime;
+    public float jumpBaseAccel = 1f;
+    public float jumpForwardAccel = 1f;
+    public float jumpBackwardAccel = 1f;
+    public float airJumpTime;
+
+    private bool jumping = false;
+    private bool walking;
+    Rigidbody frogRigidbody;
+    
+    void Start(){
+        frogRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
     void Update()
     {
         jumpingMovement();
+        if(!jumping) walkingMovement();
     }
+    private void walkingMovement(){
+        Vector3 movementVector = new Vector3();
+        Vector3 rotationVector = new Vector3();
 
+        if(Input.GetKey(KeyCode.W)) { 
+            movementVector = gameObject.transform.forward; 
+            walking = true;
+        }
+        else if(Input.GetKey(KeyCode.S)) {
+            movementVector = gameObject.transform.forward * -1f;
+            walking = true;
+        }
+        else if(Input.GetKey(KeyCode.A)) {
+            movementVector = gameObject.transform.right * -1f; 
+            walking = true;
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            movementVector = gameObject.transform.right; 
+            walking = true;
+        }
+        else walking = false;
+
+        if(!walking){
+            if(Input.GetKey(KeyCode.E)) rotationVector = new Vector3(0f, 90f, 0f);
+            else if(Input.GetKey(KeyCode.Q)) rotationVector = new Vector3(0f, -90f, 0f);
+        }
+        
+
+        gameObject.transform.position = gameObject.transform.position + (movementVector * walkingSpeed * Time.deltaTime);
+        gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles + (rotationVector * turnSpeed * Time.deltaTime));
+    }
     private void jumpingMovement(){
 
-        Vector3 jumpVector = gameObject.transform.up + (gameObject.transform.forward * 0.5f);
-        if(Input.GetKeyDown(KeyCode.Space)){
-            gameObject.GetComponent<Rigidbody>().AddForce(jumpVector * jumpingStrength);
+        Vector3 jumpVector = gameObject.transform.up + (gameObject.transform.forward * directionalJumpStrength);
+
+        if(Input.GetKey(KeyCode.Space) && !jumping){
+            jumping = true;
+            startJumpTime = Time.time;
+            maxJumpTime = startJumpTime + airJumpTime;
+
+            frogRigidbody.AddForce(jumpVector * jumpingStrength);
         }
+        else if(Input.GetKey(KeyCode.Space) && jumping && (startJumpTime + maxJumpTime > Time.time )){
+            frogRigidbody.AddForce(gameObject.transform.up * jumpBaseAccel, ForceMode.Acceleration);
+        }
+
+        if(Input.GetKey(KeyCode.W) && jumping){
+            frogRigidbody.AddForce(gameObject.transform.forward * jumpForwardAccel, ForceMode.Acceleration);
+        }
+        if(Input.GetKey(KeyCode.S) && jumping){
+            frogRigidbody.AddForce(gameObject.transform.forward * -1f * jumpForwardAccel, ForceMode.Acceleration);
+        }
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Ground") jumping = false;
     }
 }
