1b4197335e25f9c424d784aacc487804c5bae5f2
SlicedDice <60396325+SlicedDice@users.noreply.github.com>
2021-06-20 17:38:14 +0200

    Update Movement_Paper.cs

--- Assets/Scripts/Movement_Paper.cs
+++ Assets/Scripts/Movement_Paper.cs
@@ -77,6 +77,19 @@ public class Movement_Paper : MonoBehaviour
             }
         }
 
+        if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E) && !(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))){
+            float rotationMod = 1f;
+            if(Input.GetKey(KeyCode.Q)) rotationMod = -1f;
+
+            if(longSide){
+                rolledUp_Long.transform.Rotate(rotation * rotationMod, Space.World);
+                flatPaper.transform.Rotate(rotation * rotationMod, Space.World);
+            }
+            else{
+                rolledUp_Short.transform.Rotate(rotation * rotationMod, Space.World);
+                flatPaper.transform.Rotate(rotation * rotationMod, Space.World);
+            }
+        }
 
 
     }
@@ -127,7 +140,7 @@ public class Movement_Paper : MonoBehaviour
             }
         }
         else {
-            if(Input.GetKey(KeyCode.E) && (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) )){
+            if(Input.GetKey(KeyCode.T) && (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) )){
                 flatPaper.SetActive(true);
                 rolledUp_Long.SetActive(false);
                 rolledUp_Short.SetActive(false);
