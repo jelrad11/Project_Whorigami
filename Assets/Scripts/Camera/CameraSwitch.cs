5cd28d5c35cbc9043c8d476d858f8aea039cc0c6
SlicedDice <60396325+SlicedDice@users.noreply.github.com>
2021-07-04 19:56:29 +0200

    Update CameraSwitch.cs

--- Assets/Scripts/Camera/CameraSwitch.cs
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -13,7 +13,7 @@ public class CameraSwitch : MonoBehaviour
         stateSwitchAnimator = GameObject.Find("CameraSwitchController").GetComponent<Animator>();
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player"){
             switchState();
         }
